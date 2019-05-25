0360551eb2f3bc46b300633fb14bb53e668e554e
Nur Meilinda Nugraha <nmlinda97@gmail.com>
2019-05-25 13:49:06 +0700

    fix upload

--- src/pages/pembayaran/pembayaran.ts
+++ src/pages/pembayaran/pembayaran.ts
@@ -1,18 +1,13 @@
+import { Http, Headers } from '@angular/http';
 import { NativePageTransitions } from '@ionic-native/native-page-transitions';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController, AlertController, ToastController, LoadingController, ActionSheetController } from 'ionic-angular';
 import { Data } from '../../provider/data';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { DetailPesananPage } from '../detail-pesanan/detail-pesanan';
 
 import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
 import { Camera, CameraOptions } from '@ionic-native/camera';
-/**
- * Generated class for the PembayaranPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -43,6 +38,7 @@ export class PembayaranPage {
     public alertCtrl: AlertController,
     private camera: Camera,
     private transfer: FileTransfer,
+    public Http: Http,
     public viewCtrl: ViewController) {
     this.idPesanan = this.navParams.get('id');
     this.harga = this.navParams.get('harga');
@@ -98,8 +94,8 @@ export class PembayaranPage {
 
       // this.img = 'data:image/jpeg;base64,' + result;
 
-      this.postPhoto(result);
-
+      // this.postPhoto(result);
+      this.sendImage(result);
       this.validPhoto = true;
 
     }
@@ -120,7 +116,8 @@ export class PembayaranPage {
       // this.uploadFoto();
 
       // this.img = 'data:image/jpeg;base64,' + imageData;
-      this.postPhoto(imageData);
+      // this.postPhoto(imageData);
+      this.sendImage(imageData);
 
       this.validPhoto = true;
     }, (err) => {
@@ -129,8 +126,45 @@ export class PembayaranPage {
     });
   }
 
+  sendImage(img) {
+    console.log('img: ', img)
+
+    this.data.getData().then((data) => {
+      // var headers = new Headers();
+      // let token = data.api_token;
+      // headers.append("Authorization", "Bearer " + token);
+      // var url = this.data.BASE_URL + "/uploadBuktiPembayaran/" + this.idPesanan;
+      // var response = this.Http
+      //   .post(url, img, { headers: headers })
+      //   .map(res => res.json());
+      // console.log('response: ', response)
+      // loading.dismiss();
+      // return response;
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': "multipart/form-data",
+          'Authorization': 'Bearer ' + data.api_token
+        })
+      };
+      let imgs: FormData = new FormData();
+      imgs.append('img', img);
+    this.httpClient.post(this.data.BASE_URL +"/uploadBuktiPembayaran/" + this.idPesanan, imgs, httpOptions).subscribe(data => {
+      let response = data;
+      console.log(response);
+    });
+  });
+  }
+
 
   postPhoto(img) {
+
+    if (img) {
+      console.log('ada foto')
+      console.log(img)
+    } else {
+      console.log('no foto')
+    }
+
     const fileTransfer: FileTransferObject = this.transfer.create();
 
     let loading = this.loadCtrl.create({
@@ -144,25 +178,20 @@ export class PembayaranPage {
         fileKey: 'img',
         fileName: img.substr(img.lastIndexOf('/') + 1),
         chunkedMode: false,
+        mimeType: "multipart/form-data",
         headers: {
-          'Content-Type': 'image/jpeg',
           'Authorization': 'Bearer ' + data.api_token
         }
       }
-      if (img) {
-        console.log('ada foto')
-        console.log(img)
-      } else {
-        console.log('no foto')
-      }
-      fileTransfer.upload(img,encodeURI(this.data.BASE_URL + "/uploadBuktiPembayaran/" + this.idPesanan), options, true)
+      fileTransfer.upload(img, encodeURI(this.data.BASE_URL + "/uploadBuktiPembayaran/" + this.idPesanan), options, true)
         .then((response) => {
           this.responses = response;
           this.upload = JSON.parse(this.responses.response);
           console.log(this.upload)
 
+          loading.dismiss();
+
           if (this.upload.Status == 200) {
-            loading.dismiss();
             this.NativePageTransitions.fade(null);
             let currentIndex = this.navCtrl.getActive().index;
             this.navCtrl.push(DetailPesananPage, { data: this.idPesanan }).then(() => {
@@ -185,39 +214,30 @@ export class PembayaranPage {
             alert.present();
 
           } else {
-            loading.dismiss();
-            let alert = this.alertCtrl.create({
-              title: 'Unggah Bukti Pembayaran Gagal',
-              message: 'Silahkan coba lagi.',
-              buttons: [
-                {
-                  text: 'OK',
-                  handler: () => {
-                    console.log('Agree clicked');
-                  }
-                }
-              ]
-            });
-            alert.present();
+            this.alertGagal();
           }
 
         }, (err) => {
           loading.dismiss();
           console.log(err);
-          let alert = this.alertCtrl.create({
-            title: 'Unggah Bukti Pembayaran Gagal',
-            message: 'Silahkan coba lagi.',
-            buttons: [
-              {
-                text: 'OK',
-                handler: () => {
-                  console.log('Agree clicked');
-                }
-              }
-            ]
-          });
-          alert.present();
+          this.alertGagal();
         });
     })
+  }
+  alertGagal() {
+
+    let alert = this.alertCtrl.create({
+      title: 'Unggah Bukti Pembayaran Gagal',
+      message: 'Silahkan coba lagi.',
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 }
